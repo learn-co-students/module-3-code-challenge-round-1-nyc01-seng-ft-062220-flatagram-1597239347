761305205fadceb4cb8935133b6a917b90955684
Aiannn <soktoevayan@gmail.com>
2020-08-12 10:07:42 -0400

    1st deliverable done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 // write your code here
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function getImageData() {
+        fetch(`http://localhost:3000/images/1`)
+        .then(response => response.json())
+        .then(picData => {
+            renderImageData(picData)
+        })
+    }
+
+    const imgTag = document.querySelector('.image')
+    const picDataDiv = document.querySelector('.image-card')
+    function renderImageData(data) {
+        imgTag.src = data.image 
+        picDataDiv.children[0].innerText = data.title
+        picDataDiv.children[2].children[0].innerText = data.likes+' likes'
+        picDataDiv.children[3].children[0].innerText = data.comments[0].content
+        picDataDiv.children[3].children[1].innerText = data.comments[1].content
+        picDataDiv.children[3].children[2].innerText = data.comments[2].content
+    }
+
+
+
+
+
+
+
+    getImageData()
+
+})
