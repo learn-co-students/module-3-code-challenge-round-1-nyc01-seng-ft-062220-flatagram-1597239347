f7f93e1a40357068f21baa8b84c7ad1eabd967e3
yulia <juliana.ny2008@gmail.com>
2020-08-12 11:28:10 -0400

    3rd commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
      const image = document.querySelector(".image")
      const span = document.querySelector(".likes")
-     const comm = document.querySelector(".comments")
-     const listUl = document.getElementsByTagName( "li")
-    
+     //const comm = document.querySelector(".comments")
+     //const listUl = document.getElementsByTagName( "li")
+     const heart = document.querySelector(".like-button")
 
-     
+
          fetch( "http://localhost:3000/images/1")
          .then(resp => resp.json())
          .then(obj => { renderImg(obj)
@@ -14,10 +14,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
          function renderImg(obj){
              image.src = obj.image 
-             span.innerHTML = obj.likes
-             console.log(listUl[1].innerHTML)
-           
+             span.innerHTML = `${obj.likes} likes`
+             
          }
+
+         heart.addEventListener("click", function(e) { 
+                let options = {
+                    method: "PATCH",
+                    headers: {
+                      "Content-Type": "application/json",
+                      "Accept": "application/json"
+                    },
+                    body: JSON.stringify({                  
+                    })
+                }
+            
+              fetch ( "http://localhost:3000/images/1" + options)
+              .then(res => {res.json()})
+              .then(resp=> {console.log(resp)})
+        })
+
+         
+
+
         
         
 
