8567ecb0e0a7c4f7e7879f49d4201b077b8d37aa
dolly-d <dolly.desir@gmail.com>
2020-08-12 10:28:28 -0400

    commit message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // write your code here
 document.addEventListener("DOMContentLoaded", ()=> {
-    //  See the image received from the server, including its title, 
-    // likes and comments when the page loads.
+    //  ** See the image received from the server, including its title, 
+    // ** likes and comments when the page loads.
 
     //  Click on the heart icon to increase image likes, 
     // and still see them when I reload the page
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
     //  Add a comment (no persistance needed)
 
     const url = 'http://localhost:3000/images/1'
+    
+    
 
     function getDoggo(){
         fetch(url)
@@ -19,7 +21,23 @@ document.addEventListener("DOMContentLoaded", ()=> {
     function renderDoggo(doggo){
         const dogTitle = document.querySelector('.title')
         const dogImage = document.querySelector('.image')
-        console.log(dogTitle)
+        let dogLikes = document.querySelector('.likes')
+        // put comments in array to filter
+        let allComments = doggo.comments
+        // console.log(allComments)
+        dogTitle.innerText = doggo.title
+        dogImage.src = doggo.image
+        dogLikes.innerText = doggo.likes + ' Likes'
+        let dogComments = document.querySelector('.comments')
+        dogComments.innerText = ""
+
+        allComments.forEach(comment => {
+            const commentsLi = document.createElement('li')
+            const commentsContent = comment.content
+            commentsLi.innerText = commentsContent
+            dogComments.append(commentsLi)
+        })
+        
     }
 
     getDoggo()
