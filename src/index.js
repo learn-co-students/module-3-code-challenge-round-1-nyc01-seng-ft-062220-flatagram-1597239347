01435d01cd59c0bf38d84a2a79a08594a7b377ec
yulia <juliana.ny2008@gmail.com>
2020-08-12 10:08:03 -0400

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', function() {
+
+     const image = document.querySelector(".image")
+     const span = document.querySelector(".likes")
+     const comm = document.querySelector(".comments")
+
+     
+         fetch( "http://localhost:3000/images/1")
+         .then(resp => resp.json())
+         .then(obj => { renderImg(obj)
+          })
+
+         function renderImg(obj){
+             image.src = obj.image 
+             span.innerHTML = obj.likes
+             
+
+         }
+        
+
+})
+
