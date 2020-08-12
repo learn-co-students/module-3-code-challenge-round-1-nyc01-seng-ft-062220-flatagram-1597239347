11572a70a4ea336376697a40a933ef67c658175d
roman9891 <roman9891@gmail.com>
2020-08-12 10:07:39 -0400

    title, image, likes rendered

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// write your code here
+const baseUrl = `http://localhost:3000`
+
+document.addEventListener(`DOMContentLoaded`, e => {
+    const renderComment = comment => {
+        
+    }
+    const renderImageData = imageData => {
+        const imageContainer = document.querySelector(`.image-container`)
+        const title = imageContainer.querySelector(`.title`)
+        const image = imageContainer.querySelector(`img`)
+        const likes = imageContainer.querySelector(`span`)
+
+        imageContainer.dataset.id = imageData.id
+        title.innerText = imageData.title
+        image.src = imageData.image
+        likes.innerText = `${imageData.likes} likes`
+        imageData.comments.forEach(comment => renderComment(comment))
+        console.log(imageData.comments)
+    }
+
+    fetch(baseUrl + `/images/1`)
+    .then(r => r.json())
+    .then(imageData => renderImageData(imageData))
+})
