0a4d43f12595e20803104035b6e10a507c1ea0ce
github username <github email address>
2020-08-12 11:34:16 -0400

    adv  deliverable, delete comment

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 
 /* 
-- See the image received from the server, including its title, likes and comments when the page loads
-- Click on the heart icon to increase image likes, and still see them when I reload the page
-- Add a comment (no persistance needed)
-- Downvote an image
-- Still see the comments written after reloading the page
-  > For this one, you want to make a POST request to the `/comments` endpoint.
-  > Your comment object must have an `imageId` key with a value of `1` for it to work.
-*/
-
-/*
-- GET `/images/1`
-- PATCH `/images/1`
-- POST `/comments`
-- DELETE `/comments/:id`
+- √ See the image received from the server, including its title, likes and comments when the page loads
+- √ Click on the heart icon to increase image likes, and still see them when I reload the page
+- √ Add a comment (no persistance needed)
+- √ Downvote an image
+- √ Still see the comments written after reloading the page
+        > For this one, you want to make a POST request to the `/comments` endpoint.
+        > Your comment object must have an `imageId` key with a value of `1` for it to work.
+- √ Delete a comment
+        > To persist this, you will have to make a DELETE request to the `/comments/:id` endpoint.
 */
 
 document.addEventListener("DOMContentLoaded", e=>{
     
     const page_link = "http://localhost:3000/images/1"
-    const comment_link = "http://localhost:3000/comments"
+    const comment_link = "http://localhost:3000/comments/"
     const ul = document.querySelector(".comments")
     const button = document.querySelector(".like-button")
 
@@ -44,6 +39,7 @@ document.addEventListener("DOMContentLoaded", e=>{
             page.comments.forEach (comment=>{
 
                 let li = document.createElement('li')
+                li.dataset.id = comment.id
                 li.innerText = comment.content
                 ul.appendChild(li)
             })
@@ -73,57 +69,71 @@ document.addEventListener("DOMContentLoaded", e=>{
 
         })//button EventListener
     }//f upVoteImage
-
-    upVoteImage()
-
-    let form = document.querySelector(".comment-form") 
-    form.addEventListener("submit", e=>{
-
-        e.preventDefault()
-
-        let newComment = e.target.comment.value
-        let li = document.createElement('li')
-        li.innerText = newComment
-        ul.appendChild(li)
-        form.reset()
+ 
+    function addComment(){
+        let form = document.querySelector(".comment-form") 
+        form.addEventListener("submit", e=>{
+            
+            e.preventDefault()
+            let newComment = e.target.comment.value
+            let li = document.createElement('li')
+             li.innerText = newComment
+             ul.appendChild(li)
+             form.reset()
+             
+             let body = {imageId:1, content: newComment}
+             
+             let options = {
+                 method: "POST",
+                 headers: {
+                     "content-type": "application/json",
+                     "accept": "application/json"
+                    },
+                    body: JSON.stringify(body)
+                }
+            fetch(comment_link, options)
+                
+            })//formEventListener
+        }//f addComment
         
-
-        let body = {
-            imageId:1,
-            content: newComment}
-
-        let options = {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "accept": "application/json"
-            },
-            body: JSON.stringify(body)
-        }
-        fetch(comment_link, options)
+        function downVoteImage(){
+            
+            let downVoteButton = document.createElement('button')
+            downVoteButton.innerHTML = `♡`
+            downVoteButton.className ="unlike"
+            
+            let likeSectionDiv = document.querySelector(".likes-section")
+            likeSectionDiv.insertBefore(downVoteButton, button)
+            
+            let downVote = document.querySelector(".unlike")
+            downVote.addEventListener("click", e=>{
+                
+                let span = document.querySelector(".likes")
+                let likesNumber = parseInt(span.innerText.split(" ")[0])
+                let newLikes = likesNumber-1
+                span.innerText = `${newLikes} likes`
+            })
+        }//f downVoteLikes
         
-
-    })//formEventListener
-
-    function downVoteImage(){
-
-        let downVoteButton = document.createElement('button')
-        downVoteButton.innerHTML = `♡`
-        downVoteButton.className ="unlike"
+        //when click on comment - it's been deleted
+        function deleteComment(){
+            
+           ul.addEventListener("click", e=>{
+               let commentId = parseInt(e.target.dataset.id)
+               let li = document.querySelector(`[data-id="${commentId}"]`)
+                let options = {
+                    method: "DELETE"
+                }
+                fetch(comment_link+commentId, options)
+                .then(resp=>[
+                    li.remove()
+                ])  
+            })
+        } //f deleteComment
         
-        let likeSectionDiv = document.querySelector(".likes-section")
-        likeSectionDiv.insertBefore(downVoteButton, button)
-         
-        let downVote = document.querySelector(".unlike")
-        downVote.addEventListener("click", e=>{
-    
-            let span = document.querySelector(".likes")
-            let likesNumber = parseInt(span.innerText.split(" ")[0])
-            let newLikes = likesNumber-1
-            span.innerText = `${newLikes} likes`
-        })
-    }//f downVoteLikes
-
-    downVoteImage()
-
-})//DOMContentLoaded
+        upVoteImage()
+        addComment()
+        downVoteImage()
+        deleteComment()
+        
+    })//DOMContentLoaded
