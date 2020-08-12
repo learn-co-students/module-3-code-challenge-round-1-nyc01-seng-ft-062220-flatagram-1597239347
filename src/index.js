7539fad36103680401bf484be1c390a149e2daec
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-12 11:10:28 -0400

    Done_Base_Deliv

--- src/index.js
+++ src/index.js
@@ -5,33 +5,48 @@ const COMMENT_URL = "http://localhost:3000/comments"
 
 function init() {
     getFlataGram();
+    getComments();
 };
 
 const renderFlataGram = (gram) => {
+    const imageCard = document.querySelector(".image-card")
+    imageCard.dataset.imageId = `${gram.id}`
     const gramTitle = document.querySelector("h2.title")
     const gramImage = document.querySelector("img.image")
     const gramLikes = document.querySelector("span.likes")
     gramTitle.innerHTML = `${gram.title}`
     gramImage.src = `${gram.image}`
     gramLikes.innerHTML = `${gram.likes} likes`
-
-    const commnentsUl = document.querySelector("ul.comments")
-
-    gramCommentHandler(commnentsUl, gram.comments);
-    submitHandler(gram.id);
+    
     likeHandler(gram)
 };
 
-const gramCommentHandler = (ul, comments) => {
-    ul.innerHTML = ""
+const likeHandler = (gram) => {
+    document.addEventListener("click", e => {
+        if (e.target.matches(".like-button")){
+            gram.likes += 1
+           
+            patchFlataGram(gram)
+            .then(resp => {
+                getFlataGram()
+            })
+        }
+    })
+};
+
+const gramCommentHandler = (comments) => {
+    const commnentsUl = document.querySelector("ul.comments")
+    commnentsUl.innerHTML = ""
     for (let comment of comments){
         const commentLi = document.createElement("li")
         commentLi.innerText = `${comment.content}`
-        ul.append(commentLi)
+        commnentsUl.append(commentLi)
     };
 };
 
-const submitHandler = (imageId) => {
+const submitHandler = () => {
+    const imageCard = document.querySelector(".image-card")
+    imageId = imageCard.dataset.imageId
     document.addEventListener("submit", e => {
         e.preventDefault()
         const form = e.target
@@ -43,29 +58,11 @@ const submitHandler = (imageId) => {
         postComment(contentObj)
         .then(resp => {
             form.reset()
-            getFlataGram()
+            getComments()
         })
-
     });
 };
-
-const likeHandler = (gram) => {
-    document.addEventListener("click", e => {
-        if (e.target.matches(".like-button")){
-            console.log(gram)
-        }
-    })
-};
-
-
-
-
-
-
-
-
-
-
+submitHandler();
 
 //fetch functions
 
@@ -74,6 +71,24 @@ const getFlataGram = (contentObj) => {
     .then(resp => resp.json())
     .then(renderFlataGram)
 };
+
+const getComments = () => {
+    fetch(COMMENT_URL)
+    .then(resp => resp.json())
+    .then(gramCommentHandler)
+};
+
+const patchFlataGram = (newGram) => {
+    const options = {
+        method: "PATCH",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(newGram)
+    };
+
+    return fetch(BASE_URL, options)
+}
 
 const postComment = (contentObj) => {
     const options = {
