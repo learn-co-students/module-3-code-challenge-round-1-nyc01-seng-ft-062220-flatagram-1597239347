78d2b1f4540e1e13d71f32b2fb53e3e11afa9a34
“jzhouerm” <jzhouerm@hotmail.com>
2020-08-12 10:27:51 -0400

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 document.addEventListener("DOMContentLoaded", e => {
-const baseUrl = "http://localhost:3000/images/1"
+const baseUrl = "http://localhost:3000/images/"
 
 function renderImage(image) {
 const title = document.querySelector("body > div > div > h2")
 title.innerText = `${image.title}`
-console.log(title);
 const photo = document.querySelector("body > div > div > img")
+photo.src = `${image.image}`
 const likes = document.querySelector("body > div > div > div > span")
+likes.innerText = `${image.likes} likes`
 const likeBtn = document.querySelector("body > div > div > div > button")
 const commentsUl = document.querySelector("body > div > div > ul") //append comments in li
-
 
 }
 
 function getImages(){
-    fetch("baseUrl")
+    fetch(baseUrl)
     .then(response => response.json())
     .then(images => images.forEach(image => renderImage(image)))
   }
 
+  //submit listener---------------------------------------------------------------------------------
+
+  document.addEventListener("submit", e => {
+  e.preventDefault()
+  const form = document.querySelector(".form")
+  form = e.target
+  const postBtn = document.querySelector("body > div > div > form > button")
+  const commentLine = document.querySelector("body > div > div > form > input")
+
+  
+
+  
+
+  })
+
+  //click listener---------------------------------------------------------------------------------
+  document.addEventListener("click", e => {
+  
+  })
 
 
 
