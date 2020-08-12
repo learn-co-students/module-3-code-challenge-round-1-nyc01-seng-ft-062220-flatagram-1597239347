919f466be8d9f3939cf7292df9d38df68c85452a
vakas-786 <vakas0959@gmail.com>
2020-08-12 10:47:58 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -6,30 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(imageURL)
     .then(response => response.json())
     .then(image => {
-            image.forEach(image => renderPost(image))
-            console.log(image)
-        })
-
-    fetch(commentURL)
-    .then(response => response.json())
-    .then(comments => {
-        comments.forEach(renderPost)
+        image.forEach(renderPost)
     })
+
+    // fetch(commentURL)
+    // .then(response => response.json())
+    // .then(comments => {
+    //     comments.forEach(renderPost)
+    // })
 
     //fetch the comments and images they're on seperate URLs and render them to the post 
 
     const renderPost = (image, comments) => {
-        image.forEach(imageArray => )
-       const imagePost = document.getElementsByClassName("image").src
-       const title = document.querySelector("body > div > div > h2")
-       const likes = document.querySelector("body > div > div > div > span")
-       const commentUl = document.getElementById('comments')
-       const commentLi = document.createElement('li')
-       commentLi.innerText = comments.content
-       imagePost.src = images.image
-       console.log(image)
-
-
+            
+            const imagePost = document.getElementsByClassName("image").src
+            const title = document.querySelector("body > div > div > h2")
+            const likes = document.querySelector("body > div > div > div > span")
+            const commentUl = document.getElementById('comments')
+            const commentLi = document.createElement('li')
+            commentLi.innerText = comments.content
+            console.dir(image)
 
     }
 
